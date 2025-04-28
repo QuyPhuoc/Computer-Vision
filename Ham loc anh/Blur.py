cea68c45209441f5b6b0b851e6fa833acd5bf953
QuyPhuoc <quyphuoc2109@gmail.com>
2025-04-28 16:33:31 +0700

    update

--- Ham loc anh/Blur.py
+++ Ham loc anh/Blur.py
@@ -1,7 +1,7 @@
 import cv2
 
 img = cv2.imread(r'C:\Anhdep\trump.jpg')
-
+# Bộ lọc trung bình: img_blur = cv2.blur(img, (ksize,ksize))
 img_new = cv2.blur(img, (5,5))
 cv2.imshow('New', img_new)
 cv2.waitKey(0)
